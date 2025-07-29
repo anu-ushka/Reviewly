2530057cc92524b683d48c251a65a0ac266621f9
anu-ushka <anushkatyagi.2013@gmail.com>
2025-07-29 10:52:44 +0530

    fixed scroll

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -7,11 +7,12 @@ import Services from './pages/Services';
 import Blog from './pages/Blog';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-
+import ScrollToTop from './ScrollToTop'; 
 
 function App() {
   return (
-    <div>
+    <Router> 
+      <ScrollToTop />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -21,7 +22,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
-    </div>
+    </Router>
   );
 }
 
